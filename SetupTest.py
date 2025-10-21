03f09b7ddb31e6bf9b66d03364e0101f6f8bfc7b
ShumpsTech <jlfuturistictech@hotmail.com>
2025-10-21 15:54:10 +0800

    basic commit with added new lines of codes
    This is hello from Jiale. I am adding those new line of codes here.

---
+++ SetupTest.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+# --- Configuration ---
+URL = "https://practice.qabrains.com/"
+
+#@pytest.fixture(scope="function")
+def setup():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)  # Implicit wait for 10 seconds
+    driver.get(URL)
+    return driver
